8a01dab5cde3b3df2d8f9dc71925ab0c181235df
damiendsl <damiendsl@yahoo.fr>
2018-03-28 22:11:28 +0000

    before errors  validation displyed in form

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,11 +1,23 @@
 class CategoriesController < ApplicationController
     def index
-        
+        @categories = Categorie.all
+    end
+    def show
+        @categorie = Categorie.find(params[:id])
     end
     def new
     
     end
     def create
-        render plain: params[:post].inspect
+        #render plain: params[:categorie].inspect
+        @categorie = Categorie.new(categorie_params)
+        if(@categorie.save)
+            redirect_to @categorie
+        else
+            render 'new'
+        end
+    end
+    private def categorie_params
+        params.require(:categorie).permit(:title)
     end
 end
---
+++ app/models/categorie.rb
@@ -0,0 +1,4 @@
+class Categorie < ActiveRecord::Base
+    validates :title,presence: true,
+                length:{minimum: 2}
+end
