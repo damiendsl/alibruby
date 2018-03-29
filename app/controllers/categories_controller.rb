ceaa88448a343411f9fd5b3adfdf428d9e746a6a
damiendsl <damiendsl@yahoo.fr>
2018-03-29 05:30:53 +0000

    errors ok

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,21 +3,21 @@ class CategoriesController < ApplicationController
         @categories = Categorie.all
     end
     def show
-        @categorie = Categorie.find(params[:id])
+        @category = Categorie.find(params[:id])
     end
     def new
-    
+        @category=Categorie.new
     end
     def create
         #render plain: params[:categorie].inspect
-        @categorie = Categorie.new(categorie_params)
-        if(@categorie.save)
-            redirect_to @categorie
+        @category = Categorie.new(category_params)
+        if(@category.save)
+            redirect_to @category
         else
             render 'new'
         end
     end
-    private def categorie_params
-        params.require(:categorie).permit(:title)
+    private def category_params
+        params.require(:category).permit(:title)
     end
 end
